37a19dda733a2c52e48de085c8d12c815529f5c2
nawodawi <nawodawijebandara7@gmail.com>
2020-06-22 01:40:12 +0530

    updated R file

--- dynamic_data_scraping.R
+++ dynamic_data_scraping.R
@@ -1,3 +1,19 @@
-
-
 library(RSelenium)
+library(XML)
+library(janitor)
+library(lubridate)
+library(magrittr)
+library(dplyr)
+
+
+remDr <- remoteDriver(port = 4445L)
+remDr$open()
+
+remDr$navigate("https://www.worldometers.info/coronavirus/")
+remDr$getTitle()
+remDr$screenshot(display = TRUE)
+tableElem <- remDr$findElement(using = "id", "main_table_countries_today")
+tableElem 
+doc <- htmlParse(remDr$getPageSource()[[1]])
+table_tmp <- readHTMLTable(doc)
+table_tmp
